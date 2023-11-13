d29529aa00a6a771b670e2298c3bb98bca0887ec
Erick Kleim <erick.kleim@gmail.com>
2023-11-13 12:44:30 -0300

    add InterfacesStatics

--- src/main/java/java8/interfacesStatics/InterfacesStaticsMain.java
+++ src/main/java/java8/interfacesStatics/InterfacesStaticsMain.java
@@ -1,5 +1,9 @@
 package java8.interfacesStatics;
 
 public class InterfacesStaticsMain {
-
+	public static void main(String[] args) {
+		
+		System.out.println(OneInterface.duplicate("Word"));
+		//System.out.println(OneInterface.triplicate("Word"));
+	}
 }
--- src/main/java/java8/interfacesStatics/OneInterface.java
+++ src/main/java/java8/interfacesStatics/OneInterface.java
@@ -1,5 +1,15 @@
 package java8.interfacesStatics;
 
 public interface OneInterface {
-
+	static String duplicate(String simple) {
+		return simple+simple;
+	}
+	
+	/*
+	 * Métodos defaul: evita a necessidade de implementar o método em todas as
+	 * classes que implementam esta interface.
+	 */
+	default String triplicate(String simple) { 
+		return simple+simple+simple;
+	}
 }
